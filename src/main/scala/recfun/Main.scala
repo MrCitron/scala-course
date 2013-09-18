6d1cc12d09a0c998ff6ad8e3b83772730f2fa37e
Metin OSMAN <mosman@localhost.localdomain>
2013-09-19 01:04:08 +0200

    [MOS] Sauvegarde

---
+++ src/main/scala/recfun/Main.scala
@@ -0,0 +1,60 @@
+package recfun
+import common._
+
+object Main {
+  def main(args: Array[String]) {
+    println("Pascal's Triangle")
+    for (row <- 0 to 10) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+  }
+
+  /**
+   * Exercise 1
+   */
+  def pascal(c: Int, r: Int): Int = {
+    if (c == 0) 1
+    else if (r < 2) 1
+    else pascal(c - 1, r - 1) + pascal(c, r - 1)
+  }
+
+  /**
+   * Exercise 2
+   */
+  def balance(chars: List[Char]): Boolean = {
+    if (chars.isEmpty) true
+    else {
+      def balanceCount(x: Int, chars: List[Char]): Boolean = {
+        if (x < 0) false
+        else if (chars.isEmpty) x == 0
+        else if (chars.head == '(') balanceCount(x + 1, chars.tail)
+        else if (chars.head == ')') balanceCount(x - 1, chars.tail)
+        else balanceCount(x, chars.tail)
+      }
+      balanceCount(0, chars)
+    }
+  }
+
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (money == 0) 0
+    else if (coins.isEmpty) 0
+    else {
+      def countSimpleDivider(count: Int, coins: List[Int]): Int = {
+        if (coins.isEmpty) count
+        else {
+          val h = coins.head
+          val t = coins.tail
+          if (money < h) 0
+          else if (money % h == 0) countSimpleDivider(count + 1, t)
+          else countSimpleDivider(count, t)
+        }        
+      }
+      countSimpleDivider(0, coins.sorted)
+    }
+  }
+}
