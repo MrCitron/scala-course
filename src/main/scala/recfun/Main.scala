8019372afa8997cd756566b0431f39e7147ac42f
Metin OSMAN <mosman@localhost.localdomain>
2013-09-19 01:24:43 +0200

    Correction pascal

--- src/main/scala/recfun/Main.scala
+++ src/main/scala/recfun/Main.scala
@@ -15,8 +15,7 @@ object Main {
    * Exercise 1
    */
   def pascal(c: Int, r: Int): Int = {
-    if (c == 0) 1
-    else if (r < 2) 1
+    if (c == 0 || r < 2 || c == r) 1
     else pascal(c - 1, r - 1) + pascal(c, r - 1)
   }
 
